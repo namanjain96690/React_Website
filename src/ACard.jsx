5f6df862df4853f843f0f3058c0e023529c67e6c
naman <jain96690@gmail.com>
2022-01-28 14:51:54 +0530

    about

---
+++ src/ACard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+function ACard(){
+return(
+    <>
+<div className="col-md-8 col-lg-6 col-10 mx-auto mb-3">
+<div className="carde">
+<div className="imgBox">
+<img src="img.jpg" />
+</div>
+<div className="content">
+<h2>Smiling World</h2>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga aspernatur consequuntur soluta illo eum sit amet accusamus perferendis veniam esse consectetur iste magni, neque, sequi nesciunt rerum voluptas possimus. Aspernatur.
+Quo, esse cupiditate repellendus aliquam sit fugiat minus, veritatis dolor repellat adipisci perspiciatis hic ea officia, ipsam excepturi voluptate voluptatem. Assumenda earum rem obcaecati soluta perferendis doloremque, esse magnam aut.</p>
+</div>
+</div>
+</div>
+</>
+);
+}
+export default ACard;
--- src/About.jsx
+++ src/About.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import Common from './Common';
-import about from '../src/img/about.jpg';
+import './About.css'; 
+import ACard from './ACard';
 function About() {
     return (
-      <Common 
-      name='Welcome To About Page' 
-      dest='/contact'
-      imgsrc={about}
-      visit='/contact'
-      btnname='Contact Us'
-
-      />    
+    <>
+    <div className="my-5">
+        <div className="text-center">
+          <h1>About Me</h1>
+        </div>
+        <div className="container-fluid mt-5 about">
+        <div className="row">
+        <div className=" col-12 col-sm-10 col-lg-8 mx-auto">
+              <div className="row gy-5 gx-5 d-flex justify-content-center">
+            <ACard />
+            <ACard />
+      </div>
+      </div>
+      </div>
+      </div>
+     </div>
+    </>
       );
   }
   
